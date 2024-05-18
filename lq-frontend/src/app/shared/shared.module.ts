bedff5db88fe8d8f8b4ec3d9627b41e38635de6d
CristianLaverde <laverdemc77@gmail.com>
2024-05-17 21:26:11 -0500

    LQ/LQ-UPDATE-Se agrega el modulo shared con el modulo de ng zorro

---
+++ lq-frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgZorroModule } from './modules/ng-zorro.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NgZorroModule
+  ]
+})
+export class SharedModule { }
