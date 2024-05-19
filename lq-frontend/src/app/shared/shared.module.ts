fa6bfe1dcbed7805582a0c84403ddf42a8faf284
CristianLaverde <laverdemc77@gmail.com>
2024-05-18 20:22:35 -0500

    LQ/LQ-ADD-Se agrega la libreria ng zorro
    -Se agrega la librería ng zorro así como el modulo de shared en donde se encontraran módulos y componentes que se usaran en vario lugres

---
+++ lq-frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgZorroModule } from './modules/ng-zorro/ng-zorro.module'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NgZorroModule
+  ]
+})
+export class SharedModule { }
