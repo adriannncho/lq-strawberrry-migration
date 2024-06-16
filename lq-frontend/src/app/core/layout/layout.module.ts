4d474286cb94d3eb685ec332db641b0067e8323d
CristianLaverde <laverdemc77@gmail.com>
2024-06-16 16:09:43 -0500

    UPDATE-LQ-00005-Se actualizan las impotatciones

--- lq-frontend/src/app/core/layout/layout.module.ts
+++ lq-frontend/src/app/core/layout/layout.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { RouterModule } from '@angular/router';
 
 import { NgZorroModule } from '../../shared/modules/ng-zorro.module';
 import { BaseComponent } from './components/base/base.component';
@@ -12,7 +13,8 @@ import { BaseComponent } from './components/base/base.component';
   ],
   imports: [
     CommonModule,
-    NgZorroModule
+    NgZorroModule,
+    RouterModule
   ]
 })
 export class LayoutModule { }
