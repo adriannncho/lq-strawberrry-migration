ab1366b72a40f383c28b331ee68c801664df1170
CristianLaverde <laverdemc77@gmail.com>
2024-07-27 09:19:55 -0500

    UPDATE-LQ-00004-Se ajusta routing y modulos

--- lq-frontend/src/app/modules/intranet/home/home.module.ts
+++ lq-frontend/src/app/modules/intranet/home/home.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeIntranetComponent } from './pages/home-intranet/home-intranet.component';
+import { HomeRoutingModule } from './home-routing.module'
+import { NgZorroModule } from 'src/app/shared/modules/ng-zorro.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeIntranetComponent
+  ],
   imports: [
-    CommonModule
+    NgZorroModule,
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
--- lq-frontend/src/app/modules/intranet/intranet.module.ts
+++ lq-frontend/src/app/modules/intranet/intranet.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IntranetRoutingModule } from './intranet-routing.module'
+import { NgZorroModule } from 'src/app/shared/modules/ng-zorro.module';
+import { HomeModule } from './home/home.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    NgZorroModule,
+    CommonModule,
+    IntranetRoutingModule,
+    HomeModule
   ]
 })
 export class IntranetModule { }
