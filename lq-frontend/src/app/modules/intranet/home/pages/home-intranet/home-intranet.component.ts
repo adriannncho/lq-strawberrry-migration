fc7e3e3b1951304ea1725524480769ca22480e25
CristianLaverde <laverdemc77@gmail.com>
2024-07-28 21:38:33 -0500

    UPDATE-LQ-00004-Se ajustan el home

--- lq-frontend/src/app/modules/intranet/home/pages/home-intranet/home-intranet.component.ts
+++ lq-frontend/src/app/modules/intranet/home/pages/home-intranet/home-intranet.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Product, ProductMap } from 'src/app/core/models/order-products/products-interface';
+import { ProductsOrderService } from 'src/app/core/services/products-order/products-order.service';
+import { SizeProducts } from 'src/app/core/utilities/utilities-interfaces';
 
 @Component({
   selector: 'app-home-intranet',
@@ -6,16 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-intranet.component.css']
 })
 export class HomeIntranetComponent {
+  products!: Product[]; 
+  productsMap!: ProductMap[];
+  sizes = SizeProducts;
+  loadingProducts: boolean = false;
+  blockTabToppings: boolean = true;
+  selectedIndex: number = 0;
+  renderToppings: boolean = false;
+  productSendTab!: ProductMap;
 
-  tabsList = [
-    {
-      name: 'Productos',
-      icon: 'coffee'
-    },
-    {
-      name: 'Toppings',
-      icon: 'shop'
-    }
-  ]
+  order = []
 
+  constructor(
+    private productsService : ProductsOrderService,
+  ) {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.loadingProducts = true;
+    this.productsService.getAllProducts().subscribe(res => {
+      if(res) {
+        this.products = res;
+        this.productsMap = this.createObjProducts(this.products);
+        this.loadingProducts = false;
+      }
+    })
+  }
+
+  createObjProducts(products: Product[]): ProductMap[] {
+    let productsForMap: ProductMap[];
+    productsForMap = products.map(item => {
+      let size;
+      if(item.size === 1) {
+        size = this.sizes._ID_ONE_;
+      }else if (item.size === 2) {
+        size = this.sizes._ID_TWO_;
+      }
+
+      return {
+        idProduct: item.idProduct,
+        name: item.name,
+        description: item.description,
+        price: item.value,
+        sizeMap: size ? size : this.sizes._ID_ONE_,
+        quantitySauces: item.quantitySalsa,
+        quantityToppingsClasic: item.quantityClasic,
+        quantityToppingsPremium: item.quantityPremium,
+        size: item.size,
+        detailProduct: item.detailProduct
+      }
+    });
+    return productsForMap
+  }
+
+  addProduct(product: ProductMap) {
+    this.productSendTab = product;
+    this.selectedIndex = 1;
+    this.blockTabToppings = false;
+    this.renderToppings = true;
+  }
 }
