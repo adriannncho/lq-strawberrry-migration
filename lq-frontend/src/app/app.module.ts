0a4eea9547c490e28652cde913039c76573d1737
adrianherrera23 <adrian2005herrera@gmail.com>
2024-08-07 17:36:30 -0500

    FIX-LQ Se regresan los cambios y se agrega local strategy

--- lq-frontend/src/app/app.module.ts
+++ lq-frontend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { registerLocaleData } from '@angular/common';
+import { registerLocaleData, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import localeEsCO from '@angular/common/locales/es-CO'; // Importar locale
-
 import {HttpClientModule} from "@angular/common/http";
+import {es_ES, NZ_DATE_LOCALE, NZ_I18N} from 'ng-zorro-antd/i18n';
+import {es as DateES} from 'date-fns/locale';
 
 registerLocaleData(localeEsCO, 'es-CO');
 
@@ -22,8 +23,11 @@ registerLocaleData(localeEsCO, 'es-CO');
     BrowserAnimationsModule
   ],
   providers: [
+    { provide: NZ_I18N, useValue: es_ES },
     { provide: LOCALE_ID, useValue: 'es-CO' },
+    { provide: NZ_DATE_LOCALE, useValue: DateES },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'COP' },
+    { provide: LocationStrategy, useClass:HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
