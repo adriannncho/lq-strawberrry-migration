697b15e70cc19c02eb39679c9ce236f4cf744f09
CristianLaverde <laverdemc77@gmail.com>
2024-08-10 16:05:09 -0500

    UPDATE-LQ-Importacion

--- lq-frontend/src/app/modules/intranet/users/users.module.ts
+++ lq-frontend/src/app/modules/intranet/users/users.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TableUsersComponent } from './components/table-users/table-users.component';
 import { UsersRoutingModule } from './users-routing.module';
+import { UsersComponent } from './pages/users/users.component';
 
 
 @NgModule({
   declarations: [
+    UsersComponent,
     TableUsersComponent,
   ],
   imports: [
