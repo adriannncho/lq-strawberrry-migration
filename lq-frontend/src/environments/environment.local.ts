fccccdd2e05d39f9b5289722db61c99539d2b417
Adrian Herrera <118476922+adriannncho@users.noreply.github.com>
2025-01-12 21:48:42 -0500

    Update environment.local.ts

--- lq-frontend/src/environments/environment.local.ts
+++ lq-frontend/src/environments/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrlsLQ: {
-    lq_internal: ' https://lq-fresa-internal-f6lmcxzrea-uw.a.run.app/internal'
+    lq_internal: ' https://lq-fresa-internal-937148703478.us-central1.run.app'
   },
   environmentName: 'local',
   production: false,
